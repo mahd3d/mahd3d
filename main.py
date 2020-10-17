584cb5e27e646ea771f3e6c281210df612b736a7
quassy <quassy.github.io@gmail.com>
2020-10-17 11:49:20 +0200

    Massive speed improvement, remove data from repo

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 import plotly.graph_objects as go
 import pandas as pd
 import plotly.express as px
-from typing import Union, Optional
+from typing import Union, Optional, Tuple
+from time import time
 
 
 class Line:
@@ -34,20 +35,55 @@ class Sphere:
         return f"(x-{s.x})²+(y-{s.y})²+(y-{s.y})² = {s.r**2}"
 
 
-def load_e57(file: str = "data/raw/CustomerCenter1 1.e57") -> dict:
+def timeit(func):
+    def timed(*args, **kw):
+        start = time()
+        result = func(*args, **kw)
+        end = time()
+        duration = end - start
+        print(f"{func.__name__}() took {duration} seconds")
+        return result
+
+    return timed
+
+
+@timeit
+def load_e57(
+    file: str = "data/raw/CustomerCenter1 1.e57",
+) -> Tuple[dict, dict]:
     """Return a dictionary with the point types as keys."""
     print(f"Loading e57 file {file}.")
     e57 = pye57.E57(file)
+
+    # Get and clean-up header
+    raw_header = e57.get_header(0)
+    header = {}
+    for attr in dir(raw_header):
+        if attr[0:1].startswith("_"):
+            continue
+        try:
+            value = getattr(raw_header, attr)
+        except pye57.libe57.E57Exception as e:
+            continue
+        else:
+            header[attr] = value
+
+    header["pos"] = e57.scan_position(0)
+
     data = e57.read_scan_raw(0)
+    # for key, values in data.items():
+    #     assert isinstance(values, np.ndarray)
+    #     assert len(values) == 151157671
+    #     print(f"len of {key}: {len(values)} ")
 
-    for key, values in data.items():
-        assert isinstance(values, np.ndarray)
-        print(f"len of {key}: {len(values)} ")
-
-    return data
+    return data, header
 
 
-def get_points(data: dict, step: int = 1000) -> pd.DataFrame:
+@timeit
+def get_points(
+    data: dict,
+    step: int = 1000,
+) -> pd.DataFrame:
     x = data["cartesianX"][::step]
     y = data["cartesianY"][::step]
     z = data["cartesianZ"][::step]
@@ -56,67 +92,82 @@ def get_points(data: dict, step: int = 1000) -> pd.DataFrame:
     b = data["colorBlue"][::step]
     i = data["intensity"][::step]
 
-    points: pd.DataFrame = pd.DataFrame(data={"x": x, "y": y, "z": z, "r": r, "g": g, "b": b, "i": i})
+    points: pd.DataFrame = pd.DataFrame(
+        data={"x": x, "y": y, "z": z, "r": r, "g": g, "b": b, "i": i}
+    )
 
-    return points
-
-
-def get_points_with_computed(points: pd.DataFrame) -> pd.DataFrame:
     points["rgba"] = points.apply(compute_rgba, axis=1)
-    points["roof"] = points.apply(compute_roof, axis=1)
-    points["floor"] = points.apply(compute_floor, axis=1)
 
     return points
 
 
-def compute_rgba(row) -> str:
+@timeit
+def get_points_with_computed(
+    points: pd.DataFrame,
+) -> pd.DataFrame:
+    points["roof"] = False
+    points.loc[points["z"] > 3.4, ["roof"]] = True
+    points["floor"] = False
+    points.loc[points["z"] < -1.5, ["floor"]] = True
+    # points["roof"] = points.apply(compute_roof, axis=1)
+    # points["floor"] = points.apply(compute_floor, axis=1)
+
+    return points
+
+
+def compute_rgba(
+    row,
+) -> str:
     if "a" not in row:
         row["a"] = 1.0
     return f'rgba({row["r"]}, {row["g"]}, {row["b"]}, {row["a"]:.2f})'
 
 
-def compute_rgba_from_count(row) -> str:
-    if "a" not in row:
-        row["a"] = 1.0
-    return f'rgba({row["r"]}, {row["g"]}, {row["b"]}, {row["a"]:.2f})'
-
-
-def compute_roof(row) -> bool:
-    return row["z"] > 3.4
-
-
-def compute_floor(row) -> bool:
-    return row["z"] < -1.5
-
-
-def compute_alpha(row) -> float:
+def compute_alpha(
+    row,
+) -> float:
     if row["roof"] or row["floor"]:
         return 0.3
     else:
         return 1.0
 
 
-def plot_histograms(points: pd.DataFrame) -> bool:
+def plot_histograms(
+    points: pd.DataFrame,
+) -> bool:
     for col in points:
         fig = px.histogram(points, x=col)
         fig.show()
     return True
 
 
-def plot_3d(x: pd.Series, y: pd.Series, z: pd.Series, text: Union[None, pd.Series, pd.Index], color: [None, pd.Series],) -> None:
-    fig = go.Figure(data=[go.Scatter3d(
-        x=x,
-        y=y,
-        z=z,
-        text=x.index if text is None else text,
-        mode='markers',
-        marker=dict(
-            size=8,
-            color=z if color is None else color,  # set color to an array/list of desired values
-            colorscale='Viridis',  # choose a colorscale
-            opacity=0.8,
-        )
-    )])
+@timeit
+def plot_3d(
+    x: pd.Series,
+    y: pd.Series,
+    z: pd.Series,
+    text: Union[None, pd.Series, pd.Index],
+    color: [None, pd.Series],
+) -> None:
+    fig = go.Figure(
+        data=[
+            go.Scatter3d(
+                x=x,
+                y=y,
+                z=z,
+                text=x.index if text is None else text,
+                mode="markers",
+                marker=dict(
+                    size=8,
+                    color=z
+                    if color is None
+                    else color,  # set color to an array/list of desired values
+                    colorscale="Viridis",  # choose a colorscale
+                    opacity=0.8,
+                ),
+            )
+        ]
+    )
 
     # tight layout
     fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
@@ -132,30 +183,28 @@ def main() -> None:
     # Detail size, smaller is more detailed but slower
     # 1000 is recommended for displaying with Plotly, 300 is the minimum
     step: int = 3000
+    filename = f"data/computed/points_{step}.v2.df.feather"
 
     try:
-        print(f"Trying to use existing points with step size of {step} from saved points file.")
-        # p = pd.read_pickle(f"data/computed/points_{step}.df.pickle")
-        # p = pd.read_hdf(f"data/computed/points_{step}.df.hdf", "df")
-        # p = pd.read_parquet(f"data/computed/points_{step}.df.parquet")
-        p = pd.read_feather(f"data/computed/points_{step}.df.feather")
-    except Exception as e:
+        print(
+            f"Trying to use existing points with step size of {step} from saved points file."
+        )
+        p = pd.read_feather(filename)
+    except FileNotFoundError as e:
         print(f"Couldn't get points with step size of {step}, loading raw data.")
-        data: dict = load_e57()
+        data, header = load_e57()
         p: pd.DataFrame = get_points(data, step)
-        # p.to_pickle(f"data/computed/points_{step}.df.pickle")
-        # p.to_hdf(f"data/computed/points_{step}.df.hdf", "df")
-        # p.to_parquet(f"data/computed/points_{step}.df.parquet")
-        p.to_feather(f"data/computed/points_{step}.df.feather")
+        p.to_feather(filename)
     finally:
         print("p loaded")
 
-    plot_histograms(p)
+    # plot_histograms(p)
 
     p["0"] = 0
     p = get_points_with_computed(p)
 
-    p = p[~p["roof"] & ~p["floor"]]
+    # p = p[~p["roof"] & ~p["floor"]]
+    p = p[~p["roof"]]
     # points = points[(points["z"] >= -1.5) & (points["z"] <= -1.0)]
     # points["a"] = points.apply(compute_alpha, axis=1)
 
@@ -170,6 +219,7 @@ def main() -> None:
     f = p[p["floor"]].copy()
     f["count"] = 0
     import itertools
+
     grid: int = 1
     counts = []
     for x, y in itertools.product(
@@ -177,10 +227,17 @@ def main() -> None:
         np.arange(min(f["y"]), max(f["y"]), grid),
     ):
         overscan = 0.0
-        dots = f[(f["x"] >= x-overscan) & (f["x"] < x+1+overscan) & (f["y"] >= y-overscan) & (f["y"] < y+1+overscan)]
+        dots = f[
+            (f["x"] >= x - overscan)
+            & (f["x"] < x + 1 + overscan)
+            & (f["y"] >= y - overscan)
+            & (f["y"] < y + 1 + overscan)
+        ]
         count = len(dots)
         counts += [count]
-        f.loc[(f["x"] >= x) & (f["x"] < x + 1) & (f["y"] >= y) & (f["y"] < y + 1), "count"] = count
+        f.loc[
+            (f["x"] >= x) & (f["x"] < x + 1) & (f["y"] >= y) & (f["y"] < y + 1), "count"
+        ] = count
         # print(f"{x}, {y}: {count}")
 
     f.loc[f["count"] > 20, "count"] = 20
